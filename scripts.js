2f4d40550956c3b7ca467125b60ff534d2aba7bb
Sabrina Harvey <harveysabrina81@gmail.com>
2022-10-30 17:01:58 -0500

    Completed Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+
+    var email = document.getElementById("email");
+    var msg = document.getElementsByClassName("message")[0];
+    var subbtn = document.getElementsByTagName("button")[0];
+
+    subbtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if(email.value === "" || email.value === null)
+        {
+            msg.textContent = "Please enter a valid email address."
+        }
+
+        else
+        {
+            msg.textContent = `Thank you! Your email address, ${email.value}, has been added to our mailing list!`;
+        }
+    })
+
+}
+
+
+
